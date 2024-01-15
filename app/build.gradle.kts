609acbbd2d05e0e6ef5375079b60dec99a003de8
esatiyev <satiyev2004@gmail.com>
2024-01-16 02:26:51 +0400

    Updated design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+
     }
 
     buildTypes {
@@ -43,9 +45,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 //    implementation("androidx.customview:customview:1.1.0")
@@ -56,7 +58,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0") // For Gson converter
@@ -66,9 +68,17 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
 //    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0"
-    implementation("androidx.activity:activity-ktx:1.8.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
 //    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // google maps
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("com.google.android.libraries.places:places:3.3.0")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
